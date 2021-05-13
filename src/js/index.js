1efea27182fb739b0f5c5b725f8c94b65c5a22d9
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2021-05-13 22:26:42 +0400

    feat: add deploy

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-// Styles
-import '../assets/scss/main.scss';
-import 'animate.css';
-import WOW from 'wow.js';
-
-new WOW().init();
-
-import './components/menu-dropdown';
-import './components/loader';
