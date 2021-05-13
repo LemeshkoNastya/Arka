12e4a72183924924bd231389cd8781b57f46f417
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2021-05-13 22:45:32 +0400

    fix: change loader and color in header

--- src/js/components/loader.js
+++ src/js/components/loader.js
@@ -1,5 +1,6 @@
 // Magic string
 const classMask = '.mask';
+const classMaskHide = 'mask__hide';
 const opacityNull = '0';
 
 // Variable
@@ -8,7 +9,8 @@ const mask = document.querySelector(classMask);
 // Event
 window.addEventListener('load', () => {
     mask.style.opacity = opacityNull;
+    // mask.classList.add(classMaskHide);
     setTimeout(() => {
-        mask.remove;
+        mask.remove();
     }, 600);
 })
